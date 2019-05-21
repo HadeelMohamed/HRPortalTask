5f3db7ea3a7e3b0585c05a7e0c4afdea83b33d43
Hadeel Mohammed <hadeel.mostafa.cs@gmail.com>
2019-05-21 12:04:22 +0200

    done phase one db

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,6 +16,10 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
+            $table->string('last_name')->nullable();
+            $table->string('role');
+            $table->string('phone_number')->nullable();
+            $table->date('hiringdate');
             $table->string('email')->unique();
             $table->string('password');
             $table->rememberToken();
---
+++ database/migrations/2019_05_21_095135_create_attendance_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAttendanceTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('attendance', function (Blueprint $table) {
+            $table->increments('id');
+             $table->string('attendance_status');
+             $table->date('date');
+             $table->time('time')->nullable();
+              $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')
+                  ->references('id')->on('users')
+                  ->onDelete('cascade')
+                  ->onupdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('attendance');
+    }
+}
